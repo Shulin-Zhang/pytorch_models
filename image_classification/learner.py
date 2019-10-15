6049ad8b7a15102b47ba5e8046428cf17f9ae245
张树林 <zhangslwork@yeah.net>
2019-10-15 20:32:55 +0800

    learner

---
+++ image_classification/learner.py
@@ -0,0 +1,19 @@
+# zhangshulin
+# zhangslwork@yeah.net
+# 2019-10-14
+
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+
+class Learner:
+    def __init__(self, model):
+        self.model = model
+
+    def fit(self, dataloader, lr, epochs):
+        pass
+        
+
+
